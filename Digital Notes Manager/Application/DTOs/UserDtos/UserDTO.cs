f38897a765fcc80e33b2c5e5c2d061a06d646324
Abdalrhman Gad <126109248+Abdalrhman-Gad@users.noreply.github.com>
2025-03-06 00:05:25 +0200

    add user DTOs

---
+++ Digital Notes Manager/Application/DTOs/UserDtos/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Notes_Manager.Application.DTOs
+{
+    public class UserDTO
+    {
+        public int UserID { get; set; }
+
+        public string Username { get; set; }
+    }
+}
